cd1b7125312ab8abb6ae886fb3ff7adebe2059f0
yashp98 <35022242+yashp98@users.noreply.github.com>
2023-03-22 20:32:15 +0530

    Update GooglePlus.h

--- src/ios/GooglePlus.h
+++ src/ios/GooglePlus.h
@@ -1,15 +1,13 @@
 #import <Cordova/CDVPlugin.h>
-#import <GooglePlus/GooglePlus.h>
+#import <GoogleSignIn/GoogleSignIn.h>
 
-@interface GooglePlus : CDVPlugin<GPPSignInDelegate>
+@interface GooglePlus : CDVPlugin
 
 @property (nonatomic, copy) NSString* callbackId;
+@property (nonatomic, assign) BOOL isSigningIn;
 
 - (void) login:(CDVInvokedUrlCommand*)command;
-- (void) trySilentLogin:(CDVInvokedUrlCommand*)command;
 - (void) logout:(CDVInvokedUrlCommand*)command;
 - (void) disconnect:(CDVInvokedUrlCommand*)command;
-- (void) share_unused:(CDVInvokedUrlCommand*)command;
 
 @end
-
