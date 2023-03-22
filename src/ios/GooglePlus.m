32e51908ad3848af4f8aaba25506cde41c24d411
yashp98 <35022242+yashp98@users.noreply.github.com>
2023-03-22 20:24:16 +0530

    Update GooglePlus.m

--- src/ios/GooglePlus.m
+++ src/ios/GooglePlus.m
@@ -1,189 +1,126 @@
+#import <GoogleOpenSource/GoogleOpenSource.h>
 #import "AppDelegate.h"
-#import "objc/runtime.h"
 #import "GooglePlus.h"
+
+// need to swap out a method, so swizzling it here
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector);
+
+@implementation AppDelegate (IdentityUrlHandling)
+
++ (void)load {
+  swizzleMethod([AppDelegate class],
+                @selector(application:openURL:sourceApplication:annotation:),
+                @selector(identity_application:openURL:sourceApplication:annotation:));
+}
+
+- (BOOL)identity_application: (UIApplication *)application
+                     openURL: (NSURL *)url
+           sourceApplication: (NSString *)sourceApplication
+                  annotation: (id)annotation {
+
+  if (url && [url.path rangeOfString:@"oauth2callback"].location != NSNotFound) {
+    return [GPPURLHandler handleURL:url sourceApplication:sourceApplication annotation:annotation];
+  } else {
+    // call super
+    return [self identity_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+  }
+}
+
+@end
 
 @implementation GooglePlus
 
-- (void)pluginInitialize
-{
-    NSLog(@"GooglePlus pluginInitizalize");
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURL:) name:CDVPluginHandleOpenURLNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURLWithAppSourceAndAnnotation:) name:CDVPluginHandleOpenURLWithAppSourceAndAnnotationNotification object:nil];
-}
-
-- (void)handleOpenURL:(NSNotification*)notification
-{
-    // no need to handle this handler, we dont have an sourceApplication here, which is required by GIDSignIn handleURL
-}
-
-- (void)handleOpenURLWithAppSourceAndAnnotation:(NSNotification*)notification
-{
-    NSMutableDictionary * options = [notification object];
-
-    NSURL* url = options[@"url"];
-
-    NSString* possibleReversedClientId = [url.absoluteString componentsSeparatedByString:@":"].firstObject;
-
-    if ([possibleReversedClientId isEqualToString:self.getreversedClientId] && self.isSigningIn) {
-        self.isSigningIn = NO;
-        [[GIDSignIn sharedInstance] handleURL:url];
-    }
-}
-
-// If this returns false, you better not call the login function because of likely app rejection by Apple,
-// see https://code.google.com/p/google-plus-platform/issues/detail?id=900
-// Update: should be fine since we use the GoogleSignIn framework instead of the GooglePlus framework
-- (void) isAvailable:(CDVInvokedUrlCommand*)command {
-  CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
 - (void) login:(CDVInvokedUrlCommand*)command {
-  [[self getGIDSignInObject:command] signIn];
+  [[self getGooglePlusSignInObject:command] authenticate];
 }
 
-/** Get Google Sign-In object
- @date July 19, 2015
- */
 - (void) trySilentLogin:(CDVInvokedUrlCommand*)command {
-    [[self getGIDSignInObject:command] restorePreviousSignIn];
-}
-
-/** Get Google Sign-In object
- @date July 19, 2015
- @date updated March 15, 2015 (@author PointSource,LLC)
- */
-- (GIDSignIn*) getGIDSignInObject:(CDVInvokedUrlCommand*)command {
-    _callbackId = command.callbackId;
-    NSDictionary* options = command.arguments[0];
-    NSString *reversedClientId = [self getreversedClientId];
-
-    if (reversedClientId == nil) {
-        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Could not find REVERSED_CLIENT_ID url scheme in app .plist"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
-        return nil;
-    }
-
-    NSString *clientId = [self reverseUrlScheme:reversedClientId];
-
-    NSString* scopesString = options[@"scopes"];
-    NSString* serverClientId = options[@"webClientId"];
-    NSString *loginHint = options[@"loginHint"];
-    BOOL offline = [options[@"offline"] boolValue];
-    NSString* hostedDomain = options[@"hostedDomain"];
-
-
-    GIDSignIn *signIn = [GIDSignIn sharedInstance];
-    signIn.clientID = clientId;
-
-    [signIn setLoginHint:loginHint];
-
-    if (serverClientId != nil && offline) {
-      signIn.serverClientID = serverClientId;
-    }
-
-    if (hostedDomain != nil) {
-        signIn.hostedDomain = hostedDomain;
-    }
-
-    signIn.presentingViewController = self.viewController;
-    signIn.delegate = self;
-
-    // default scopes are email and profile
-    if (scopesString != nil) {
-        NSArray* scopes = [scopesString componentsSeparatedByString:@" "];
-        [signIn setScopes:scopes];
-    }
-    return signIn;
-}
-
-- (NSString*) reverseUrlScheme:(NSString*)scheme {
-  NSArray* originalArray = [scheme componentsSeparatedByString:@"."];
-  NSArray* reversedArray = [[originalArray reverseObjectEnumerator] allObjects];
-  NSString* reversedString = [reversedArray componentsJoinedByString:@"."];
-  return reversedString;
-}
-
-- (NSString*) getreversedClientId {
-  NSArray* URLTypes = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleURLTypes"];
-
-  if (URLTypes != nil) {
-    for (NSDictionary* dict in URLTypes) {
-      NSString *urlName = dict[@"CFBundleURLName"];
-      if ([urlName isEqualToString:@"REVERSED_CLIENT_ID"]) {
-        NSArray* URLSchemes = dict[@"CFBundleURLSchemes"];
-        if (URLSchemes != nil) {
-          return URLSchemes[0];
-        }
-      }
-    }
+  // trySilentAuthentication doesn't call delegate when it fails, so handle it here
+  if (![[self getGooglePlusSignInObject:command] trySilentAuthentication]) {
+    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no valid token"];
+    [self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
   }
-  return nil;
+}
+
+- (GPPSignIn*) getGooglePlusSignInObject:(CDVInvokedUrlCommand*)command {
+  _callbackId = command.callbackId;
+  NSDictionary* options = [command.arguments objectAtIndex:0];
+  NSString* apiKey = [options objectForKey:@"iOSApiKey"];
+  if (apiKey == nil) {
+    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"iOSApiKey not set"];
+    [self writeJavascript:[pluginResult toErrorCallbackString:_callbackId]];
+    return nil;
+  }
+  
+  GPPSignIn *signIn = [GPPSignIn sharedInstance];
+  signIn.shouldFetchGooglePlusUser = YES;
+  signIn.shouldFetchGoogleUserEmail = YES;
+  signIn.clientID = apiKey;
+  signIn.scopes = @[kGTLAuthScopePlusLogin];
+  signIn.attemptSSO = YES; // tries to use other installed Google apps
+  signIn.delegate = self;
+  return signIn;
 }
 
 - (void) logout:(CDVInvokedUrlCommand*)command {
-  [[GIDSignIn sharedInstance] signOut];
+  [[GPPSignIn sharedInstance] signOut];
   CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"logged out"];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  [self writeJavascript:[pluginResult toSuccessCallbackString:command.callbackId]];
 }
 
 - (void) disconnect:(CDVInvokedUrlCommand*)command {
-  [[GIDSignIn sharedInstance] disconnect];
+  [[GPPSignIn sharedInstance] disconnect];
   CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"disconnected"];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  [self writeJavascript:[pluginResult toSuccessCallbackString:command.callbackId]];
 }
 
 - (void) share_unused:(CDVInvokedUrlCommand*)command {
   // for a rainy day.. see for a (limited) example https://github.com/vleango/GooglePlus-PhoneGap-iOS/blob/master/src/ios/GPlus.m
 }
 
-#pragma mark - GIDSignInDelegate
-/** Google Sign-In SDK
- @date July 19, 2015
- */
-- (void)signIn:(GIDSignIn *)signIn didSignInForUser:(GIDGoogleUser *)user withError:(NSError *)error {
-    if (error) {
-        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
+#pragma mark - GPPSignInDelegate
+- (void)finishedWithAuth:(GTMOAuth2Authentication *)auth
+                   error:(NSError *)error {
+  if (error) {
+    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+    [self writeJavascript:[pluginResult toErrorCallbackString:_callbackId]];
+  } else {
+    NSString *email = [GPPSignIn sharedInstance].userEmail;
+    GTLPlusPerson *person = [GPPSignIn sharedInstance].googlePlusUser;
+    NSDictionary *result;
+    
+    if (person == nil) {
+      result = @{
+                 @"email" : email
+                 };
     } else {
-        NSString *email = user.profile.email;
-        NSString *idToken = user.authentication.idToken;
-        NSString *accessToken = user.authentication.accessToken;
-        NSString *refreshToken = user.authentication.refreshToken;
-        NSString *userId = user.userID;
-        NSString *serverAuthCode = user.serverAuthCode != nil ? user.serverAuthCode : @"";
-        NSURL *imageUrl = [user.profile imageURLWithDimension:120]; // TODO pass in img size as param, and try to sync with Android
-        NSDictionary *result = @{
-                       @"email"           : email,
-                       @"idToken"         : idToken,
-                       @"serverAuthCode"  : serverAuthCode,
-                       @"accessToken"     : accessToken,
-                       @"refreshToken"    : refreshToken,
-                       @"userId"          : userId,
-                       @"displayName"     : user.profile.name       ? : [NSNull null],
-                       @"givenName"       : user.profile.givenName  ? : [NSNull null],
-                       @"familyName"      : user.profile.familyName ? : [NSNull null],
-                       @"imageUrl"        : imageUrl ? imageUrl.absoluteString : [NSNull null],
-                       };
-
-        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
+      result = @{
+                 @"email"       : email,
+                 @"displayName" : person.displayName ?: [NSNull null],
+                 @"gender"      : person.gender ?: [NSNull null],
+                 @"imageUrl"    : (person.image != nil && person.image.url != nil) ? person.image.url : [NSNull null],
+                 @"givenName"   : (person.name != nil && person.name.givenName != nil) ? person.name.givenName : [NSNull null],
+                 @"middleName"  : (person.name != nil && person.name.middleName != nil) ? person.name.middleName : [NSNull null],
+                 @"familyName"  : (person.name != nil && person.name.familyName != nil) ? person.name.familyName : [NSNull null]
+                 };
     }
+    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
+    [self writeJavascript:[pluginResult toSuccessCallbackString:_callbackId]];
+  }
 }
 
-/** Google Sign-In SDK
- @date July 19, 2015
- */
-- (void)signIn:(GIDSignIn *)signIn presentViewController:(UIViewController *)viewController {
-    self.isSigningIn = YES;
-    [self.viewController presentViewController:viewController animated:YES completion:nil];
-}
-
-/** Google Sign-In SDK
- @date July 19, 2015
- */
-- (void)signIn:(GIDSignIn *)signIn dismissViewController:(UIViewController *)viewController {
-    [self.viewController dismissViewControllerAnimated:YES completion:nil];
-}
+#pragma mark Swizzling
 
 @end
+
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector) {
+  Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
+  Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
+  
+  // If the method doesn't exist, add it.  If it does exist, replace it with the given implementation.
+  if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
+    class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
+  } else {
+    method_exchangeImplementations(destinationMethod, sourceMethod);
+  }
+}
